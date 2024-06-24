1419d102d7e02c49d3c8bc4c36d649fab1636f7f
sanket.ujgare-coditas <sanket.ujgare@coditas.com>
2024-06-24 17:31:26 +0530

    Created CustomerSales Component

---
+++ front-end/src/components/CustomerSales/CustomerSales.tsx
@@ -0,0 +1,7 @@
+import styles from "./CustomerSales.module.scss"; 
+import { CustomerSalesProps } from "./CustomerSales.types.ts" 
+ 
+const CustomerSales = ({}: CustomerSalesProps) => { 
+} 
+ 
+export default CustomerSales 
